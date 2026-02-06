e1f53df3eaa3af23f7fd795e98319a426debeb85
Andrew Nesbitt <andrewnez@gmail.com>
2026-02-06 10:40:16 +0000

    Replace internal enrichment with github.com/git-pkgs/enrichment

--- cmd/licenses.go
+++ cmd/licenses.go
@@ -10,7 +10,7 @@ import (
 	"time"
 
 	"github.com/git-pkgs/git-pkgs/internal/database"
-	"github.com/git-pkgs/git-pkgs/internal/enrichment"
+	"github.com/git-pkgs/enrichment"
 	"github.com/git-pkgs/git-pkgs/internal/git"
 	"github.com/git-pkgs/purl"
 	"github.com/git-pkgs/spdx"
